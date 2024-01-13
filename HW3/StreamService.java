67e59c48dbfdb79aabd839dccddb8f7965162e5e
«AnastasiyaLebedeva» <lmehanik72@mail.ru>
2024-01-13 21:02:44 +0300

    задание 3

---
+++ HW3/StreamService.java
@@ -0,0 +1,15 @@
+package HW3;
+
+import java.util.List;
+
+public class StreamService {
+    private List<Stream> listStreams;
+
+    public StreamService(List<Stream> listStreams) {
+        this.listStreams = listStreams;
+    }
+
+    public void sortStudentsStreamBySize(){
+        listStreams.sort(new StreamComparator());
+    }
+}
